22a28b1bc2bae95b64beea9b97d516641fdf9f36
Ado <amoadh70@gmail.com>
2026-01-01 21:08:51 +0400

    config: Allow `o-flock-app.onrender.com` for Vite dev server.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ["o-flock-app.onrender.com"],
       },
       plugins: [react()],
       define: {
